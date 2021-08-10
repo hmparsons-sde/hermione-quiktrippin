9c5124a7aa830981091e873597e4c52925cb87fe
Mitchell-Crumbley <mitchellcrumbley@gmail.com>
2021-08-09 20:10:59 -0500

    Added a separate class for each section of the main menu and simple console writes for the menu options.

---
+++ FirstGroupProject/FirstGroupProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstGroupProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mmmm, yes. Lets talk money. You run this town. Lets look at the districts!\n\x1B[4m");
+
+            Console.WriteLine("Please choose an option:\x1B[0m");
+            Console.WriteLine("1- Enter District Sales");
+            Console.WriteLine("2- Generate District Report");
+            Console.WriteLine("3- Add New Employee");
+            Console.WriteLine("4- Add a Store/District");
+            Console.WriteLine("5- Exit");
+
+        }
+    }
+}
