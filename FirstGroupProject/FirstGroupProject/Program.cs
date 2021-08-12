392558e39f09e32f311fa2124e535a9d7bfc5c3c
Rob Cole <robertncole1@gmail.com>
2021-08-12 19:13:57 -0400

    Added in new feature for adding in employee

--- FirstGroupProject/FirstGroupProject/Program.cs
+++ FirstGroupProject/FirstGroupProject/Program.cs
@@ -10,6 +10,8 @@ namespace FirstGroupProject
         static void Main(string[] args)
         {
             int storeNumber;
+            string name;
+            string jobTitle;
             Console.WriteLine("Mmmm, yes. Lets talk money. You run this town. Lets look at the districts!\n\x1B[4m");
 
             Console.WriteLine("Please choose an option:\x1B[0m");
@@ -31,8 +33,48 @@ namespace FirstGroupProject
                     break;
                 case "3":
                     Console.WriteLine("We're adding an employee");
+                    Console.Write("What is their name?");
+                    name = Console.ReadLine();
+                    bool startLoop = true;
+                    while (startLoop)
+                    {
+                        Console.WriteLine("What is their job title?\n\x1B[4m ");
+                        Console.WriteLine("1. District Manager\x1B[0m");
+                        Console.WriteLine("2. Store Manager");
+                        Console.WriteLine("3. Assistant Manager");
+                        Console.WriteLine("4. Associate");
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                jobTitle = "District Manager";
+                                Console.WriteLine(jobTitle);
+                                startLoop = false;
+                                break;
+                            case "2":
+                                jobTitle = "Store Manager";
+                                Console.WriteLine(jobTitle);
+                                startLoop = false;
+                                break;
+                            case "3":
+                                jobTitle = "Assistant Manager";
+                                Console.WriteLine(jobTitle);
+                                startLoop = false;
+                                break;
+                            case "4":
+                                jobTitle = "Associate";
+                                Console.WriteLine(jobTitle);
+                                startLoop = false;
+                                break;
+                            default:
+                                Console.WriteLine("That is not a valid answer!");
+                                break;
+
+                        }
+                    }
                     Console.Write("What store number? ");
                     storeNumber = Int32.Parse(Console.ReadLine());
+                    var Employee1 = new Employee();
+                    va.Add(Employee1);
                     var associatedStores = new Employee();
                     associatedStores.ListEmployees(storeNumber);
                     break;
