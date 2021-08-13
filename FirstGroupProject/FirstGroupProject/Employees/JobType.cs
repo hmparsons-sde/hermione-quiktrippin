c58c6926ff4e288004a9d069d1db3e619cebb591
katherinevfry <katyvfry@gmail.com>
2021-08-12 19:18:19 -0500

    add todos

---
+++ FirstGroupProject/FirstGroupProject/Employees/JobType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGroupProject.Employees
+{
+    public enum JobType
+    {
+        StoreManager,
+        AssistantManager,
+        Asscoiate
+    }
+}
--- FirstGroupProject/FirstGroupProject/Stores/Stores.cs
+++ FirstGroupProject/FirstGroupProject/Stores/Stores.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FirstGroupProject.Districts;
 
 namespace FirstGroupProject.Stores
 {
     class Stores
     {
+        //TODO: add a group of associates, store manager and assistant manager. 
+        public int StoreNumber { get; set; }
+        public DistrictType DistrictHouse { get; set; }
+
+        public Stores(int storeNumber, DistrictType districtHouse)
+        {
+            StoreNumber = storeNumber;
+            DistrictHouse = districtHouse;
+        }
+
     }
 }
